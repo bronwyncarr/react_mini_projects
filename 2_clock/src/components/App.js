529662dbbe163d0f32d9a28e30b84d50577f2317
bronwyncarr <bronwynacarr@gmail.com>
2020-12-17 23:31:00 +1100

    adds lifecycle and ticking seconds motion

--- 2_clock/src/components/App.js
+++ 2_clock/src/components/App.js
@@ -3,55 +3,84 @@ import Clock from "./Clock";
 
 class App extends Component {
   constructor(props) {
-		super(props);
-		//initialise state
+    super(props);
+    //initialise state
     this.state = {
       latitude: null,
-			longitude: null,
-			errorMsg: ""
-		}
-		
-		// user setState to set the state obj value of latitude and longitude to the position coords.lat/long
-		window.navigator.geolocation.getCurrentPosition(
-					position => this.setState({
-						latitude: position.coords.latitude,
-						longitude: position.coords.longitude
-					}),
-					error => this.setState({errorMsg: error.message})
-				)
-	
-		}
-		
-		getClockIcon() {
-			const {latitude} = this.state
-			const month = new Date().getMonth()
-			if ((month < 3 && latitude < 0) || ( (5 < month) && (month < 9) && latitude > 0)) {
-				return	"summer.png"
-			}	else if (((2 < month && month < 6) && latitude < 0) || ((8 < month) && (month < 12) && 0 < latitude)) {
-				return	"autumn.png"
-			}	else if (((5 < month) && (month < 9) && latitude < 0) || (month < 3 && latitude > 0)) {
-				return	"winter.png"
-			}	else {
-				return "spring.png"
-			}
-		}
+      longitude: null,
+      errorMsg: "",
+      date: new Date(),
+    };
+  }
 
-		render() {
-			// destructure state
-			const { latitude } = this.state
-			const { longitude } = this.state 
-			const { errorMsg } = this.state 
-			return(
-				<div>
-					<h1>Location and Time </h1>
-					{/* short ciruit logic to show error msg or location */}
-					{errorMsg || <h4>Your location is {latitude} latitude, {longitude} longitude</h4>}
-					<Clock
-						date={new Date()} 
-						icon={latitude ? this.getClockIcon() : null}
-					/>
-				</div>
-			)
+  // Season logic based on month and hemisphere
+  getClockIcon() {
+    const { latitude } = this.state;
+    const month = new Date().getMonth();
+    if (
+      (month < 3 && latitude < 0) ||
+      (5 < month && month < 9 && latitude > 0)
+    ) {
+      return "summer.png";
+    } else if (
+      (2 < month && month < 6 && latitude < 0) ||
+      (8 < month && month < 12 && 0 < latitude)
+    ) {
+      return "autumn.png";
+    } else if (
+      (5 < month && month < 9 && latitude < 0) ||
+      (month < 3 && latitude > 0)
+    ) {
+      return "winter.png";
+    } else {
+      return "spring.png";
+    }
+  }
+
+  // Gets updated time
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  componentDidMount() {
+    // user setState to set the state obj value of latitude and longitude to the position coords.lat/long
+    window.navigator.geolocation.getCurrentPosition(
+      (position) =>
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        }),
+      (error) => this.setState({ errorMsg: error.message })
+    );
+  }
+
+  // Calls tick() min every second
+  componentDidUpdate(prevState) {
+    if (prevState.date !== this.state.date) {
+      this.timerId = setInterval(() => this.tick(), 1000);
+    }
+  }
+
+  // Clears activity from running inbackground if go to different page
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    // destructure state
+    const { latitude, longitude, errorMsg, date } = this.state;
+    return (
+      <div>
+        <h1>Location and Time </h1>
+        {/* short ciruit logic to show error msg or location */}
+        {errorMsg || (
+          latitude ? <h4>
+            Your location is: {latitude.toFixed(2)} latitude, {longitude.toFixed(2)} longitude.
+          </h4> : null
+        )}
+        <Clock date={date} icon={latitude ? this.getClockIcon() : null} />
+      </div>
+    );
   }
 }
 
