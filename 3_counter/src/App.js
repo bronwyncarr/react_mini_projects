0c038701bded7db8fe89387e6340a114b2dd62fc
bronwyncarr <bronwynacarr@gmail.com>
2020-12-24 15:03:02 +1100

    useEffect to manage lifecycle

--- 3_counter/src/App.js
+++ 3_counter/src/App.js
@@ -13,11 +13,23 @@ function App() {
     setCount(prevState => prevState -1)
   }
 
+  // Lets you perform side effects
   // specify in brackets what variable to watch for and rerender only when it changes.
   // empty array means it will only run when the page intitally loads
   useEffect(() => {
     setColor(randomcolor())
   }, [count])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(prevState => prevState + 1)
+    }, 1000)
+
+    // clean up function
+    return () => clearInterval(interval)
+  }, [])
+
+
 
   return (
     <div class="app">
