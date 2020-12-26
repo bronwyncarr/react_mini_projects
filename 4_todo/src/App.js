ad85b16bbf530aa572b65e125cf8e63b0cdd47fd
bronwyncarr <bronwynacarr@gmail.com>
2020-12-26 21:31:14 +1100

    adds state to todoData in app.js

--- 4_todo/src/App.js
+++ 4_todo/src/App.js
@@ -1,27 +1,37 @@
-import React from 'react'
-import './App.css';
-import Todo from './components/Todo';
-import TodoData from './TodoData';
+import React from "react";
+import "./App.css";
+import Todo from "./components/Todo";
+import todos from "./TodoData";
+import TodoData from "./TodoData";
 
-class App extends React.Component{
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todoData: TodoData
+    }
+  }
+
   render() {
     function displayDate() {
-      const date = new Date()
-      const day = date.getDay()
-      const daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-      const todaysDate = date.getDate()
-      const monthOfTheYear = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-      const todaysMonth = date.getMonth()
-      return `${daysOfTheWeek[day]} ${todaysDate} ${monthOfTheYear[todaysMonth]}`
+      const date = new Date();
+      const day = date.getDay();
+      const daysOfTheWeek = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+      const todaysDate = date.getDate();
+      const monthOfTheYear = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+      const todaysMonth = date.getMonth();
+      return `${daysOfTheWeek[day]} ${todaysDate} ${monthOfTheYear[todaysMonth]}`;
     }
-    
+
     return (
       <div className="todo-list">
         <h1>To Do List</h1>
         <h2>{displayDate()}</h2>
-        {TodoData.map((todo) => <Todo key={todo.item} item={todo.item} completed={todo.completed} />)}
+        {this.state.todoData.map((todo) => (
+          <Todo key={todo.item} item={todo.item} completed={todo.completed} />
+        ))}
       </div>
-    )
+    );
   }
 }
 
