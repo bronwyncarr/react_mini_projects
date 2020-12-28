391f3e402b6a957c3bbd8956f081800729170f75
bronwyncarr <bronwynacarr@gmail.com>
2020-12-28 20:26:06 +1100

    initial commit

--- 4_todo/src/TodoData.js
+++ 4_todo/src/TodoData.js
@@ -2,7 +2,9 @@ const TodoData = [
   {id: 1, item: "Learn React", completed: false},
   {id: 2, item: "50 Projects 50 days", completed: true},
   {id: 3, item: "Workbook Questions", completed: true},
-  {id: 4, item: "Revise JS async/await", completed: false}
+  {id: 4, item: "Revise JS async/await", completed: false},
+  {id: 5, item: "Intro to MERN", completed: true},
+  {id: 6, item: "Work on side project", completed: false}
 ]
 
 export default TodoData
---
+++ 5_form_practise/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css';
+import React from 'react'
+
+const users = [
+  {
+    email: "",
+    password: ""
+  },
+  {
+    email: "",
+    password: ""
+  },
+  {
+    email: "",
+    password: ""
+  }
+]
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Log in</h1>
+          {}
+      </div>
+    );
+  }
+}
+
+export default App;
