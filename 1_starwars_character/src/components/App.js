e8da06fdb23388aa3e6dfab426e226c51b799ade
bronwyncarr <bronwynacarr@gmail.com>
2020-12-29 12:10:13 +1100

    data fetched from api. images to come

--- 1_starwars_character/src/components/App.js
+++ 1_starwars_character/src/components/App.js
@@ -1,16 +1,37 @@
-import React from 'react'
-import Nav from './Nav'
-import Main from './Main'
+import React, { Component } from "react";
+import Nav from "./Nav";
+import Main from "./Main";
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      characters: [],
+    };
+  }
 
-const App = () => {
-  return (
-    <div>
-      <Nav />
-      <Main />
-    </div>
-    
-  );
+  componentDidMount() {
+    console.log("mounted");
+    fetch("https://swapi.dev/api/people")
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          characters: data.results,
+        })
+      );
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        {console.log(this.state.characters)}
+        {this.state.characters.map((character, index) => {
+          return <Main key={index} character={character} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
--- 1_starwars_character/src/components/Main.jsx
+++ 1_starwars_character/src/components/Main.jsx
@@ -1,26 +1,22 @@
 import React from 'react'
-import { characters } from '../data/starWars'
+// import characterImages from '../data/starWars'
 import "../styles/index.css";
 
-const Main = () => {
+function Main(props) {
     return(
         <main>
             <h1>Star Wars Characters</h1>
-            {characters.map((character, index) => {
-                return(
-                    <div className="container" key={index}>
-                        {character.pic ? <img src={character.pic} height="200" alt=""/> : <p>No Picture available</p>}
-                            <h2>{character.name}</h2>
-                            <p>Height: {character.height} cm</p>
-                            <p>Mass: {character.mass} kg</p>
-                            <p>Hair Color: {character.hair_color}</p>
-                            <p>Eye Color: {character.eye_color}</p>
-                            <p>Birth Year: {character.birth_year}</p>
-                            {character.gender === 'n/a' ? <p>Gender: Character is a alien</p> : <p>Gender: {character.gender}</p>}
-                        <hr />
-                    </div>
-                )
-            })}
+            <div className="container" >
+                {props.character.pic ? <img src={props.character.pic} height="200" alt=""/> : <p>No Picture available</p>}
+                    <h2>{props.character.name}</h2>
+                    <p>Height: {props.character.height} cm</p>
+                    <p>Mass: {props.character.mass} kg</p>
+                    <p>Hair Color: {props.character.hair_color}</p>
+                    <p>Eye Color: {props.character.eye_color}</p>
+                    <p>Birth Year: {props.character.birth_year}</p>
+                    {props.character.gender === 'n/a' ? <p>Gender: Character is a alien</p> : <p>Gender: {props.character.gender}</p>}
+                <hr />
+            </div>
         </main>
     )
 }
